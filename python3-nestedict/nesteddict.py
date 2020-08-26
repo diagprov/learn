992999622b351b4cab38977caeff63ad7890c1c0
Antony Vennard <antony@vennard.ch>
2020-08-26 14:46:00 +0200

    Nested dictionary update code.

--- python3-nestedict/nesteddict.py
+++ python3-nestedict/nesteddict.py
@@ -41,21 +41,55 @@ def filterdict(d, prefix=None, keyfilter=None):
                 yield [key], value
 
 
-def nesteddictupdate(d, keylist, inputdict):
+def nesteddictupdate(d, keylist, updatefunc):
 
-    keytuple = tuple(keylist[:-1])
-
-    print("Update:", keytuple)
+    keytuple = keylist[:-1]
+    replacekey = keylist[-1:][0]
+    workingdict = d
     if len(keytuple) != 0:
-        del d[tuple(keytuple)]["include"]
-        d[tuple(keytuple)] = {**d[keytuple], **inputdict}
-    else:
-        del d["include"]
-        d = {**d, **inputdict}
-    return d
-import yaml
+    
+        # python doesn't give us references to the dictionaries we overwrite
+        # this is nasty, but it's the best we can do I think.
+        # basic premise is this:
+        #
+        # 1. keep a stack of parent dictionaries for the current and key 
+        #    used to find the child dictionary.
+        # 2. iterate through the dictionary, recursively setting workingdict 
+        #    to the child.
+        # 3. delete the key we require deleting
+        # 4. merge a new dictionary into this one.
+        # 5. re-insert this child dictionary into its parent by popping the 
+        #    stack, until we hit the parent
+        # done.
+        stack = []
 
-print(testdict[("inner", "innerinner", "include")])
+        for k in keytuple:
+            stack.append((k, workingdict))
+            workingdict = workingdict[k]
+
+        # extract value
+        v = workingdict[replacekey]
+        inputdict = updatefunc(v)
+        del workingdict[replacekey]
+        workingdict = {**workingdict, **inputdict}
+
+        for parentkey, parentdict in reversed(stack):
+            parentdict[parentkey] = workingdict
+            workingdict = parentdict
+        d = workingdict    
+    else:
+        # if the replace is in the root, this is much, much easier.
+
+        v = workingdict[replacekey]
+        inputdict = updatefunc(v)
+        del workingdict[replacekey]
+        d = {**workingdict, **inputdict}
+
+    # return the result
+    return d
+
+
+import yaml
 
 print(yaml.dump(testdict))
 
@@ -64,10 +98,12 @@ for path, value in update_list:
     print(path, value)
     print("----Before")
     print(yaml.dump(testdict))
-    d = nesteddictupdate(testdict, path, replacedict)
+    d = nesteddictupdate(testdict, path, lambda x: replacedict)
     testdict = d
     print("----After")
     print(yaml.dump(testdict))
 
+print("...")
 print(yaml.dump(testdict))
+print("...")
 print(yaml.dump(d))
