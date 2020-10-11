c1ba06031441e98207a99aca1611fdd666297f28
Antony Vennard <antony@vennard.ch>
2020-10-11 16:27:50 +0200

    Update Ocaml demo now language server works.

--- ocaml-hello/hello.ml
+++ ocaml-hello/hello.ml
@@ -1,3 +1,9 @@
-let () =
-    print_string "Hello, world!\n"
 
+open Printf
+open List
+
+let () =
+    print_string "Hello, world!\n";
+    let numlist = [1;2;3;4;5;] in
+    let f elem = Printf.printf "Element %d\n" elem in
+    List.iter f numlist;
