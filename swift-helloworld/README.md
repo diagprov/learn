93e1fdbbd3ac48c1c3adfd6a81360ad314d80a67
Antony Vennard <antony@vennard.ch>
2021-06-15 19:07:14 +0200

    Add Swift Helloworld.

---
+++ swift-helloworld/README.md
@@ -0,0 +1,3 @@
+# swift-helloworld
+
+A description of this package.
---
+++ swift-helloworld/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_helloworldTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_helloworldTests.allTests()
+XCTMain(tests)
