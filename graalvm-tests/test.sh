aba514d4d82b89c9ee4d9a330ceb8e9bd7914e3c
Antony Vennard <antony@vennard.ch>
2022-04-28 17:17:03 +0200

    Experiment with GraalVM to see how it makes native images.
    Signed-off-by: Antony Vennard <antony@vennard.ch>

---
+++ graalvm-tests/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+./helloworld
+./prettyprintjson <<EOF
+{"GraalVM":{"description":"Language Abstraction Platform","supports":["combining languages","embedding languages","creating native images"],"languages": ["Java","JavaScript","Node.js", "Python", "Ruby","R","LLVM"]}}
+EOF
