c1468332ae7e395a83b0eff5bac8e36787eec731
Antony Vennard <antony@vennard.ch>
2023-02-21 14:50:44 +0100

    GTK4 Menus are still possible.
    Signed-off-by: Antony Vennard <antony@vennard.ch>

---
+++ c-gtk4/menu.c
@@ -0,0 +1,54 @@
+#include <gtk/gtk.h>
+
+static void
+quit_activated(GSimpleAction *action, GVariant *parameter, GApplication *application) {
+  g_application_quit (application);
+}
+
+static void
+app_activate (GApplication *application) {
+  GtkApplication *app = GTK_APPLICATION (application);
+  GtkWidget *win = gtk_application_window_new (GTK_APPLICATION (app));
+  gtk_window_set_title (GTK_WINDOW (win), "menu1");
+  gtk_window_set_default_size (GTK_WINDOW (win), 400, 300);
+
+  gtk_application_window_set_show_menubar (GTK_APPLICATION_WINDOW (win), TRUE);
+  gtk_window_present (GTK_WINDOW (win));
+}
+
+static void
+app_startup (GApplication *application) {
+  GtkApplication *app = GTK_APPLICATION (application);
+
+  GSimpleAction *act_quit = g_simple_action_new ("quit", NULL);
+  g_action_map_add_action (G_ACTION_MAP (app), G_ACTION (act_quit));
+  g_signal_connect (act_quit, "activate", G_CALLBACK (quit_activated), application);
+
+  GMenu *menubar = g_menu_new ();
+  GMenuItem *menu_item_menu = g_menu_item_new ("Menu", NULL);
+  GMenu *menu = g_menu_new ();
+  GMenuItem *menu_item_quit = g_menu_item_new ("Quit", "app.quit");
+  g_menu_append_item (menu, menu_item_quit);
+  g_object_unref (menu_item_quit);
+  g_menu_item_set_submenu (menu_item_menu, G_MENU_MODEL (menu));
+  g_menu_append_item (menubar, menu_item_menu);
+  g_object_unref (menu_item_menu);
+
+  gtk_application_set_menubar (GTK_APPLICATION (app), G_MENU_MODEL (menubar));
+}
+
+#define APPLICATION_ID "com.github.ToshioCP.menu1"
+
+int
+main (int argc, char **argv) {
+  GtkApplication *app;
+  int stat;
+
+  app = gtk_application_new (APPLICATION_ID, G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "startup", G_CALLBACK (app_startup), NULL);
+  g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
+
+  stat =g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+  return stat;
+}
